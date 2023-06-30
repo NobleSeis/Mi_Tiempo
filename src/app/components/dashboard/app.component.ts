5339ad22646b77f52e79fb8e4302a7f1107672ad
NobleSeis <jose.gisla@gmail.com>
2023-06-30 17:34:25 +0200

    [Update] - Ahora se geolocaliza mediante la api del navegador

--- src/app/components/dashboard/app.component.ts
+++ src/app/components/dashboard/app.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { finalize, switchMap } from 'rxjs';
+import { finalize } from 'rxjs';
 
 import { Weather } from 'src/app/models/weather';
-import { IpService } from 'src/app/servicios/ip.service';
 import { WeatherService } from 'src/app/servicios/weather-service.service';
 
 @Component({
@@ -15,20 +14,30 @@ export class AppComponent implements OnInit {
   data: Weather = new Weather();
   isLoading = false;
 
-  constructor(
-    private weatherService: WeatherService,
-    private ipService: IpService
-  ) {}
+  constructor(private weatherService: WeatherService) {}
   ngOnInit(): void {
     this.isLoading = true;
-    this.ipService
-      .getIp()
-      .pipe(
-        finalize(() => (this.isLoading = false)),
-        switchMap((data: { ip: string }) =>
-          this.weatherService.getWeatherForecast(data.ip, '3', 'yes')
-        )
-      )
+
+    // La geolocalización solo funciona desde servidores firmados
+    navigator.geolocation.getCurrentPosition(
+      (success) => {
+        this.loadData(
+          `${success.coords.latitude} ${success.coords.longitude}`,
+          '7',
+          'no'
+        );
+      },
+      (error) => {
+        console.log(`No fue posible obtener la localización: ${error.message}`);
+        this.loadData('Sevilla', '7', 'yes');
+      }
+    );
+  }
+
+  loadData(place: string, days: string, alerts: 'yes' | 'no') {
+    this.weatherService
+      .getWeatherForecast(place, days, alerts)
+      .pipe(finalize(() => (this.isLoading = false)))
       .subscribe((data) => (this.data = data));
   }
 }
